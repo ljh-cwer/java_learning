89696dea67f7ebee6ed53ffd645bb6b0ccd62067
ljh_111 <ljh_111@luo.com>
2022-11-11 18:25:59 +0800

    add ConstantDemo

---
+++ IdeaProjects/basic-code/day01-code/src/ljh/javaclass/day01/demo01/ConstantDemo.java
@@ -0,0 +1,14 @@
+package ljh.javaclass.day01.demo01;
+
+public class ConstantDemo {
+    /* java的常量类型：
+        整数、小数、字符、字符串、布尔、空常量
+    */
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(0.123);
+        System.out.println('S');
+        System.out.println(true);
+        System.out.println("hello, world!");
+    }
+}
