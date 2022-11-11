13089faf99f2c45c22512ad7ed4bbd6ea6487f0c
ljh_111 <ljh_111@luo.com>
2022-11-11 18:06:55 +0800

    first commit day01_demo01_HelloWorld

---
+++ IdeaProjects/basic-code/day01-code/src/ljh/javaclass/day01/demo01/HelloWord.java
@@ -0,0 +1,13 @@
+package ljh.javaclass.day01.demo01;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!");
+        System.out.println("ln会自动换行");
+        System.out.print("Hello, World!!!");
+        System.out.println("没有ln不会自动换行");
+
+
+
+    }
+}
